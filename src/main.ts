1ed5bd1ff0424e3f1b19ea93558100fda851889d
roylin1995 <bizroylin@gmail.com>
2024-06-01 00:39:21 +0800

    註冊 Font Awesome

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { fas } from '@fortawesome/free-solid-svg-icons' // solid 實心
+import { far } from '@fortawesome/free-regular-svg-icons' // regular 一般
+import { fab } from '@fortawesome/free-brands-svg-icons' // brand 品牌
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, far, fab) // 把 icon 加入 library
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
