aa141f74ea7c17f63f241aec401d688c164bd0be
roylin1995 <bizroylin@gmail.com>
2024-06-05 14:19:05 +0800

    test

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:latest as build-stage
 RUN rm -rf node_modules
 RUN npm install -g http-server
 RUN npm install -g vite
+RUN npm install -g @rollup/plugin-alias
 RUN npm install -g @fortawesome/fontawesome-svg-core
 WORKDIR /app
 COPY package*.json ./
--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import alias from '@rollup/plugin-alias'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    alias(),
   ],
   resolve: {
     alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
-      '@': '/src'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
