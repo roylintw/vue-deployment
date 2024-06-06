1bfa188d49702a0ced96b7d0ab15836bd16b2265
roylin1995 <bizroylin@gmail.com>
2024-06-06 23:42:21 +0800

    test

--- src/views/member/Login.vue
+++ src/views/member/Login.vue
@@ -75,7 +75,7 @@ async function login(){
     }, {
       headers: {
         'Authorization': `Basic ${btoa(`${username}:${password}`)}`,
-        // 'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded;charset-utf-8',
         // 'Content-Type': 'multipart/form-data',
       },
