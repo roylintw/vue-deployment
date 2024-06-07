60dea6654932a938c5936419941b8b3b51ad3c4e
roylin1995 <bizroylin@gmail.com>
2024-06-07 15:30:10 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,10 @@ export default defineConfig({
     proxy: {
       // 將所有以 /api 開頭的請求轉發到後端服務器
       '/api': {
+        // before
         // target: 'http://localhost:8080',
+        
+        // after
         target: 'https://spring-boot-deployment-1-xpf6zdjama-de.a.run.app',
         changeOrigin: true,
         secure: true,
