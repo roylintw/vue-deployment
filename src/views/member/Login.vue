b682b9f6a776ae54b7593eedc4db44940a6c04ab
roylin1995 <bizroylin@gmail.com>
2024-06-07 12:59:56 +0800

    更換 url

--- src/views/member/Login.vue
+++ src/views/member/Login.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="container">
     <div class="login-wrapper">
-      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
       <div class="header">登入</div>
       <form @submit.prevent="login">
-      <div class="form-wrapper">
-        <label for="username">帳號:</label>
-        <input type="text" class="input-item" v-model="username" placeholder="請輸入帳號/信箱/用戶名" required />
-      </div>
+        <div class="form-wrapper">
+          <label for="username">帳號:</label>
+          <input type="text" class="input-item" v-model="username" placeholder="請輸入帳號/信箱/用戶名" required />
+        </div>
         <div>
           <label for="password">密碼:</label>
-          <input id="inputPassword" type="password" class="input-item" v-model="password" placeholder="請輸入密碼" required />
-          <i id="pinUpEye" :class="[isActive ? 'fa-eye' : 'fa-eye-slash' , 'fas']"  @click="isActive=!isActive"></i>
+          <input id="inputPassword" type="password" class="input-item" v-model="password" placeholder="請輸入密碼"
+            required />
+          <i id="pinUpEye" :class="[isActive ? 'fa-eye' : 'fa-eye-slash', 'fas']" @click="isActive = !isActive"></i>
         </div>
         <button class="btn" type="submit">登入</button>
       </form>
@@ -53,10 +54,10 @@ export default defineComponent({
 });
 </script> -->
 
-<!-- 組合式API --> 
+<!-- 組合式API -->
 <script lang="ts" setup>
 import { ref, watch } from 'vue'
-import axios, {AxiosError} from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useRouter } from 'vue-router';
 let username: ''
 let password: ''
@@ -64,12 +65,13 @@ const errorMessage = ref('');
 const router = useRouter()
 const isActive = ref(false);
 
-async function login(){
+async function login() {
   console.log('login')
   // const helloResponse = axios.get('/api/');
   // console.log('helloResponse: ', (await helloResponse).data);
   try {
-    const response = await axios.post('/api/memberLogin', {
+    // const response = await axios.post('/api/memberLogin', {
+    const response = await axios.post('https://spring-boot-deployment-1-xpf6zdjama-de.a.run.app/memberLogin', {
       username: username,
       password: password,
     }, {
@@ -86,19 +88,19 @@ async function login(){
   } catch (error) {
     console.error('Error logging in', error);
     if (error instanceof AxiosError) {
-    console.error('status', Object(error).response.status);
-  }
-    
+      console.error('status', Object(error).response.status);
+    }
+
   }
 }
 
 // Watch
-watch(isActive, ()=>{
+watch(isActive, () => {
   const inputPassword = document.getElementById("inputPassword");
-  if(isActive.value === true){
-    inputPassword?.setAttribute('type','text'); // 顯示密碼
+  if (isActive.value === true) {
+    inputPassword?.setAttribute('type', 'text'); // 顯示密碼
   } else {
-    inputPassword?.setAttribute('type','password'); // 隱藏密碼
+    inputPassword?.setAttribute('type', 'password'); // 隱藏密碼
   }
 })
 </script>
@@ -106,53 +108,59 @@ watch(isActive, ()=>{
 <style scoped>
 /* Add your styles here */
 .login-wrapper {
-    background-color: #fff;
-    width: 358px;
-    height: 588px;
-    border-radius: 15px;
-    padding: 0 50px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
+  background-color: #fff;
+  width: 358px;
+  height: 588px;
+  border-radius: 15px;
+  padding: 0 50px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
+
 .header {
-    font-size: 38px;
-    font-weight: bold;
-    text-align: center;
-    line-height: 200px;
+  font-size: 38px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 200px;
 }
 
 .input-item {
-    display: block;
-    width: 100%;
-    margin-bottom: 20px;
-    border: 0;
-    padding: 10px;
-    border-bottom: 1px solid rgb(128, 125, 125);
-    font-size: 15px;
-    outline: none;
+  display: block;
+  width: 100%;
+  margin-bottom: 20px;
+  border: 0;
+  padding: 10px;
+  border-bottom: 1px solid rgb(128, 125, 125);
+  font-size: 15px;
+  outline: none;
 }
+
 .input-item:placeholder {
-    text-transform: uppercase;
+  text-transform: uppercase;
 }
+
 .btn {
-    text-align: center;
-    padding: 10px;
-    margin: 0 auto;
-    width: 100%;
-    margin-top: 40px;
-    background-image: linear-gradient(to right, #a6c1ee, #fbc2eb);
-    color: #fff;
+  text-align: center;
+  padding: 10px;
+  margin: 0 auto;
+  width: 100%;
+  margin-top: 40px;
+  background-image: linear-gradient(to right, #a6c1ee, #fbc2eb);
+  color: #fff;
 }
+
 .msg {
-    text-align: center;
-    line-height: 88px;
+  text-align: center;
+  line-height: 88px;
 }
+
 a {
-    text-decoration-line: none;
-    color: #abc1ee;
+  text-decoration-line: none;
+  color: #abc1ee;
 }
+
 /*固定眼睛 icon 位置*/
 #pinUpEye {
   position: absolute;
